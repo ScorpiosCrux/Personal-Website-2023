a3298ab219181621741b1a94916565e131fa0d10
Tyler Chen <chentyler52@gmail.com>
2023-01-14 23:20:27 -0700

    nav bar created

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,22 +4,41 @@ import styled from "@emotion/styled";
 const Navbar = () => {
 	return (
 		<Wrapper>
-			<Link href={"/"}> Tyler Chen </Link>
+			<LogoWrapper>
+				<Link href={"/"}> TYLER </Link>
+			</LogoWrapper>
 			<Links>
-				<Link href={"/"}>Home</Link>
-				<Link href={"/about"}>About</Link>
-				<Link href={"/projects"}>Projects</Link>
+				<NavLink href={"/"}>HOME</NavLink>
+				<NavLink href={"/about"}>ABOUT</NavLink>
+				<NavLink href={"/projects"}>PROJECTS</NavLink>
+				<NavLink href={"/contact"}>CONTACT</NavLink>
 			</Links>
 		</Wrapper>
 	);
 };
 
-const Logo = styled(Link)``;
-const Links = styled.div``;
+const LogoWrapper = styled.div`
+	font-weight: 600;
+	font-size: 48px;
+
+`;
+const Links = styled.div`
+	display: flex;
+	gap: 10px;
+
+`;
+
+const NavLink = styled(Link)`
+	font-weight: 200;
+	font-size: 20px;
+`;
 
 const Wrapper = styled.nav`
 	height: 72px;
 	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 20px;
 `;
 
 export default Navbar;
