baf8a6479339f55a1de2f355075f4f6aae4578bc
Tyler Chen <chentyler52@gmail.com>
2023-03-17 05:44:12 -0600

    reverted priority load on icons

--- components/core/Icon.tsx
+++ components/core/Icon.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Icon = (props: Props) => {
 	return (
 		<StyledIcon width={props.width}>
-			<Image src={props.image} alt={props.alt} priority />
+			<Image src={props.image} alt={props.alt} />
 		</StyledIcon>
 	);
 };
